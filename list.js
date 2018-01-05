97b2d3807a63a1e0f1c072ec3db3f1c3a2856ee3
wenhui liu <wenhuiliu@wenhuideMacBook-Pro.local>
2018-01-04 21:45:24 -0800

    Added Wenhui

--- list.js
+++ list.js
@@ -1,6 +1,7 @@
 const people = `
 Brett Ritter <b.ritter@neu.edu>
 Bin Shi <shi.b@husky.neu.edu>
+Wenhui Liu <liu.wenh@husky.neu.edu>
 `.split('\n').filter( truthy => truthy );
 
 if (require.main === module) {
