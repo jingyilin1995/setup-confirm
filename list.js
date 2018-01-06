89b992a1c99935b36a0ce8e3d3fa863546a7fffd
Cara Yang <31554763+carayang@users.noreply.github.com>
2018-01-06 12:24:30 -0800

    Added Chun Yang

--- list.js
+++ list.js
@@ -4,6 +4,7 @@ Bin Shi <shi.b@husky.neu.edu>
 Jingyi "Ashley" Lin <lin.jin@husky.neu.edu>
 Lulu Zhang <zhang.lulu@husky.neu.edu>
 Wenhui Liu <liu.wenh@husky.neu.edu>
+Chun Yang <yang.chun@husky.neu.edu>
 `.split('\n').filter( truthy => truthy );
 
 if (require.main === module) {
