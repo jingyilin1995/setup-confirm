2f009f81ec45d943421cf9c615f06a782f6b4112
tc25898 <740419913@qq.com>
2018-01-14 23:14:35 -0800

    Added Tianyi Cheng

--- list.js
+++ list.js
@@ -26,6 +26,7 @@ Qifei "Rose" Lu <lu.qi@husky.neu.edu>
 Yuxin Li <li.yux@husky.neu.edu>
 Hengyi "Edward" Yang <yang.heng@husky.neu.edu>
 Qianchen He <he.qian@husky.neu.edu>
+Tianyi Cheng <cheng.ti@husky.neu.edu>
 `.split('\n').filter( truthy => truthy );
 
 if (require.main === module) {
