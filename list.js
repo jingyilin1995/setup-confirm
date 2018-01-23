874ad843bc8b390eda14d67fa37efafd22bdce76
杨潇 <yangxiao@yangxiaodeMacBook-Pro.local>
2018-01-22 18:37:41 -0800

    Added XiaoYang

--- list.js
+++ list.js
@@ -38,6 +38,7 @@ Xuhan Shan <shan.xu@husky.neu.edu>
 Tianyu Hou <hou.tia@husky.neu.edu>
 Rucheng Xiao <xiao.ruc@husky.neu.edu>
 Ching-Chi "Amanda" Chang<chang.ching@husky.neu.edu>
+Xiao Yang <yang.xiao1@husky.neu.edu>
 `.split('\n').filter( truthy => truthy );
 
 
